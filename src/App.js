e88a58aadeebbc35d0f9f27af31ce12219b3af76
YTponzu <yutoponzu@gmail.com>
2024-03-28 17:09:21 +0900

    add 404 page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Header from "./components/Header";
 import TopPage from "./pages/TopPage";
 import ThreadPage from "./pages/ThreadPage";
 import CreateThreadPage from "./pages/CreateThreadPage";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<TopPage />} exact />
-        <Route path="/thread/:id" element={<ThreadPage />} exact />
-        <Route path="/create-thread" element={<CreateThreadPage />} exact />
+        <Route path="/" element={<TopPage />} />
+        <Route path="/thread/:id" element={<ThreadPage />} />
+        <Route path="/create-thread" element={<CreateThreadPage />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
