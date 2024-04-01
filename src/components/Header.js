d8c12ae6cc81e1062352f4ab2dd8be30ed2363c8
YTponzu <yutoponzu@gmail.com>
2024-04-01 17:27:37 +0900

    update: CreateThreadPage prototype

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <a href="." className="header--title">掲示板</a>
-      <a href="." className="header--newthread-btn">スレッドをたてる</a>
+      <a href="create-thread" className="header--newthread-btn">スレッドをたてる</a>
     </header>
   )
 };
--- src/pages/CreateThreadPage.js
+++ src/pages/CreateThreadPage.js
@@ -1,7 +1,63 @@
+import { useState } from "react";
+
 const CreateThreadPage = () => {
+  const [title, setTitle] = useState("");
+  const changeTitle = e => setTitle(e.target.value);
+
+  const h1Style = {
+    width: "80%",
+    maxWidth: "800px",
+    margin: "32px auto",
+    padding: "0 1rem",
+    fontsize: "40px",
+    fontWeight: "400",
+  };
+
+  const inputStyle = {
+    display: "block",
+    width: "80%",
+    maxWidth: "800px",
+    height: "89px",
+    fontSize: "30px",
+    borderRadius: "8px",
+    border: "none",
+    padding: "12px 12px",
+    margin: "0 auto",
+    marginTop: "64px",
+  };
+
+  const divStyle = {
+    width: "80%",
+    maxWidth: "800px",
+    margin: "32px auto",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+  };
+
+  const aStyle = {
+    fontSize: "24px",
+    color: "#666",
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#66d48b",
+    fontSize: "32px",
+    color: "#fff",
+    borderRadius: "10px",
+    border: "none",
+    margin: "16px",
+    padding: "16px 64px",
+  };
+
   return (
     <div>
-      <h1>Create Thread Page</h1>
+      <h1 style={h1Style}>スレッド新規作成</h1>
+      <input type="text" placeholder="スレッドタイトル" value={title} onChange={changeTitle} style={inputStyle} />
+      <div style={divStyle}>
+        <a href="." style={aStyle}>Topに戻る</a>
+        <button style={buttonStyle}>作成</button>
+      </div>
     </div>
   );
 };
