fde3eb68a1270d7bbad156ef912350cf1b800c3b
YTponzu <yutoponzu@gmail.com>
2024-04-01 16:37:36 +0900

    update: Set title link in header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <header className="header">
-      <p className="header--title">掲示板</p>
+      <a href="." className="header--title">掲示板</a>
       <a href="." className="header--newthread-btn">スレッドをたてる</a>
     </header>
   )
