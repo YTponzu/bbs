10105fec65d6006bddf4a9b63cd183b2b8166678
YTponzu <yutoponzu@gmail.com>
2024-04-01 18:05:27 +0900

    update: Change the Header component to use styled-components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
-import "../styles/Header.css";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background: #66D48B;
+  height: 130px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 64px;
+`;
+
+const StyledHeaderTitle = styled.a`
+  text-decoration: none;
+  color: #fff;
+  font-size: 40px;
+`;
+
+const StyledNewThreadBtn = styled.a`
+  color: #fff;
+  hover: { color: #ddd; }
+`;
 
 const Header = () => {
   return (
-    <header className="header">
-      <a href="." className="header--title">掲示板</a>
-      <a href="create-thread" className="header--newthread-btn">スレッドをたてる</a>
-    </header>
+    <StyledHeader>
+      <StyledHeaderTitle href=".">掲示板</StyledHeaderTitle>
+      <StyledNewThreadBtn href="create-thread">スレッドをたてる</StyledNewThreadBtn>
+    </StyledHeader>
   )
 };
 
