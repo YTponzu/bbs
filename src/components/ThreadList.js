b5b1d89cd0e59f9f64785490b53b8f9fae942b93
YTponzu <yutoponzu@gmail.com>
2024-04-01 18:37:00 +0900

    update: Change the ThreadList component to use styled-components.

--- src/components/ThreadList.js
+++ src/components/ThreadList.js
@@ -1,5 +1,34 @@
 import { useState, useEffect } from "react";
-import "../styles/ThreadList.css";
+import styled from "styled-components";
+
+const StyledThreadList = styled.div`
+  margin: 0 auto;
+  width: 80%;
+  max-width: 800px;  
+`;
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+
+const StyledLi = styled.li`
+  background-color: #fff;
+  font-weight: bold;
+  font-size: 1.2rem;
+  border-radius: 5px;
+  border: #333 1px solid;
+  padding: 1rem 1rem 2rem 1rem;
+
+  &:hover {
+    background-color: #ddd;
+  }
+`;
+
+const StyledA = styled.a`
+  color: #333;
+  text-decoration: none;
+`;
 
 const ThreadList = () => {
   const [threads, setThreads] = useState([]);
@@ -11,17 +40,17 @@ const ThreadList = () => {
   }, []);
 
   return (
-    <div className="threadlist">
-      <ul className="threadlist--ul">
+    <StyledThreadList>
+      <StyledUl>
         {threads.map((thread) => {
           return (
-            <li key={thread.id}>
-              <a href={`/thread/${thread.id}`}>{thread.title}</a>
-            </li>
+            <StyledLi key={thread.id}>
+              <StyledA href={`/thread/${thread.id}`}>{thread.title}</StyledA>
+            </StyledLi>
           );
         })}
-      </ul>
-    </div>
+      </StyledUl>
+    </StyledThreadList>
   );
 };
 
