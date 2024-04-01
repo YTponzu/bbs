aa0f4b75232abc68955d22f526cf507fdb0d8927
YTponzu <yutoponzu@gmail.com>
2024-04-01 18:57:47 +0900

    update: Change the CreateThreadPage component to use styled-components.

--- src/pages/CreateThreadPage.js
+++ src/pages/CreateThreadPage.js
@@ -1,63 +1,74 @@
 import { useState } from "react";
 
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  width: 80%;
+  max-width: 800px;
+  margin: 32px auto;
+  padding: 0 1rem;
+  font-size: 40px;
+  font-weight: 400;
+`;
+
+const StyledInput = styled.input`
+  display: block;
+  width: 80%;
+  max-width: 800px;
+  height: 89px;
+  font-size: 30px;
+  border-radius: 8px;
+  border: none;
+  padding: 12px 12px;
+  margin: 0 auto;
+  margin-top: 64px;
+`;
+
+const Wrapper = styled.div`
+  width: 80%;
+  max-width: 800px;
+  margin: 32px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledA = styled.a`
+  font-size: 24px;
+  color: #666;
+
+  &:hover {
+    color: #333;
+  }
+`;
+
+const StyledButton = styled.button`
+  background-color: #66d48b;
+  font-size: 32px;
+  color: #fff;
+  border-radius: 10px;
+  border: none;
+  margin: 16px;
+  padding: 16px 64px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #6af2a0;
+  }
+`;
+
 const CreateThreadPage = () => {
   const [title, setTitle] = useState("");
   const changeTitle = e => setTitle(e.target.value);
 
-  const h1Style = {
-    width: "80%",
-    maxWidth: "800px",
-    margin: "32px auto",
-    padding: "0 1rem",
-    fontsize: "40px",
-    fontWeight: "400",
-  };
-
-  const inputStyle = {
-    display: "block",
-    width: "80%",
-    maxWidth: "800px",
-    height: "89px",
-    fontSize: "30px",
-    borderRadius: "8px",
-    border: "none",
-    padding: "12px 12px",
-    margin: "0 auto",
-    marginTop: "64px",
-  };
-
-  const divStyle = {
-    width: "80%",
-    maxWidth: "800px",
-    margin: "32px auto",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center"
-  };
-
-  const aStyle = {
-    fontSize: "24px",
-    color: "#666",
-  };
-
-  const buttonStyle = {
-    backgroundColor: "#66d48b",
-    fontSize: "32px",
-    color: "#fff",
-    borderRadius: "10px",
-    border: "none",
-    margin: "16px",
-    padding: "16px 64px",
-  };
-
   return (
     <div>
-      <h1 style={h1Style}>スレッド新規作成</h1>
-      <input type="text" placeholder="スレッドタイトル" value={title} onChange={changeTitle} style={inputStyle} />
-      <div style={divStyle}>
-        <a href="." style={aStyle}>Topに戻る</a>
-        <button style={buttonStyle}>作成</button>
-      </div>
+      <StyledH1>スレッド新規作成</StyledH1>
+      <StyledInput type="text" placeholder="スレッドタイトル" value={title} onChange={changeTitle} />
+      <Wrapper>
+        <StyledA href=".">Topに戻る</StyledA>
+        <StyledButton>作成</StyledButton>
+      </Wrapper>
     </div>
   );
 };
