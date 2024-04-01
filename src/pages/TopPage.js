757635b1a33302b45db1c052be1a030076505e5e
YTponzu <yutoponzu@gmail.com>
2024-04-01 17:26:32 +0900

    bugfix

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -3,7 +3,7 @@ import ThreadList from "../components/ThreadList";
 const TopPage = () => {
   return (
     <div>
-      <h1 style={{ width: "80%", "max-width": "800px", fontWeight: "400", margin: "32px auto", padding: "0 1rem" }}>新着スレッド</h1>
+      <h1 style={{ width: "80%", maxWidth: "800px", fontWeight: "400", margin: "32px auto", padding: "0 1rem" }}>新着スレッド</h1>
       <ThreadList />
     </div>
   );
