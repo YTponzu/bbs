ba01ebcd73fa3b4322e8bf082fa361a334a41f35
YTponzu <yutoponzu@gmail.com>
2024-04-02 13:01:18 +0900

    update: done CreateThread.

--- src/components/CreateThreadPage/CreateThreadButton.js
+++ src/components/CreateThreadPage/CreateThreadButton.js
@@ -1,9 +1,36 @@
 import { StyledButton } from './ComponentStyle';
 
 const CreateThreadButton = ({ title }) => {
-  const onClick = () => { console.log(title);}
+  const threadTitle = title.trim();
+  const threadTitleValidate = threadTitle.length < 1 ? false : true;
+
+  const createThread = () => {
+    if (!threadTitleValidate) {
+      alert("スレッド名を入力してください");
+      return;
+    }
+
+    fetch(
+      "https://railway.bulletinboard.techtrain.dev/threads",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title })
+      }
+    ).then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to create a thread");
+      }
+      return res.json();
+    }).then((data) => {
+      window.location.href = `/thread/${data.id}`;
+    }).catch((error) => {
+      console.error(error);
+    });
+  };
+
   return (
-    <StyledButton onClick={onClick}>作成</StyledButton>
+    <StyledButton onClick={createThread}>作成</StyledButton>
   );
 }
 
