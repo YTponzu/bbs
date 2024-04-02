2742f44949e08d5429e7116a62453359c39ad1e9
YTponzu <yutoponzu@gmail.com>
2024-04-02 17:57:16 +0900

    bugfix

--- src/components/ThreadPage/Thread.js
+++ src/components/ThreadPage/Thread.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import getThreadNameById from "../../pages/scripts/getThreadNameById";
+import getThreadNameById from "../../scripts/getThreadNameById";
 
 import TwoDivisionsLayout from "./TwoDivisionsLayout";
 import PostsSide from "./PostsSide";
